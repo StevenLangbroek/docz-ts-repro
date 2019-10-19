1ce663cc2426819e7ef2d9576ffb93ec96a152df
rakannimer <rakannimer@gmail.com>
2019-10-19 11:30:45 -0500

    fix: include lower-case files in filterComponents

--- doczrc.js
+++ doczrc.js
@@ -1,4 +1,13 @@
 module.exports = {
   title: "Docz Typescript Repro",
-  typescript: true
+  typescript: true,
+  filterComponents: files => {
+    const result = files.filter(
+      filepath =>
+        /\/[A-Z]\w*\.(js|jsx|ts|tsx)$/.test(filepath) ||
+        filepath.includes(".tsx") ||
+        filepath.includes(".jsx")
+    );
+    return result;
+  }
 };
